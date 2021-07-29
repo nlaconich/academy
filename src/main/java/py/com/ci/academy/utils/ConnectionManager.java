7e196ea0a2a22d8ce72010f7e5baadf4205f0ba0
nlaconich <88150792+nlaconich@users.noreply.github.com>
2021-07-29 10:52:45 -0400

    Add files via upload

---
+++ src/main/java/py/com/ci/academy/utils/ConnectionManager.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package py.com.ci.academy.utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author nick
+ */
+public class ConnectionManager {
+
+    public static Connection getConnection() {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost/db_academy", "developer", "developer");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        if (connection != null) {
+            System.out.println("Connections ready");
+
+        } else {
+            System.out.println("Error connections");
+        }
+
+        return connection;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        if (new ConnectionManager().getConnection() != null) {
+            System.out.println("CONNECTION OK!");
+        } else {
+            System.out.println("ERROR CONNECTION");
+        }
+
+    }
+
+}
