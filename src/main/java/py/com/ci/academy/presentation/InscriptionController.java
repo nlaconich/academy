28b8bb6e4a1310489c03792482d4718397ed2ffd
nlaconich <nlaconich@consultoresinformaticos.com.py>
2021-08-17 17:49:13 -0400

    +Add Inscription controller

---
+++ src/main/java/py/com/ci/academy/presentation/InscriptionController.java
@@ -0,0 +1,8 @@
+package py.com.ci.academy.presentation;
+
+public class InscriptionController {
+    public static void main(String[] args) {
+        InscriptionUI ui = new InscriptionUI();
+        ui.registerInscription();
+    }
+}
