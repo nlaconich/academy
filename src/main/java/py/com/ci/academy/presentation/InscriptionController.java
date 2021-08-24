8d506af41f99eadf063c111da2fbe73eaf66b49a
nlaconich <nlaconich@consultoresinformaticos.com.py>
2021-08-24 17:14:09 -0400

    Add feature webCourse

--- src/main/java/py/com/ci/academy/presentation/InscriptionController.java
+++ src/main/java/py/com/ci/academy/presentation/InscriptionController.java
@@ -1,5 +1,7 @@
 package py.com.ci.academy.presentation;
 
+import py.com.ci.academy.presentation.console.InscriptionUI;
+
 public class InscriptionController {
     public static void main(String[] args) {
         InscriptionUI ui = new InscriptionUI();
