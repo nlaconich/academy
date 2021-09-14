c78190821536ae9a1afbfe9c9a5c83d114238fd0
jespinolas <jespinola@consultoresinformaicos.com.py>
2021-09-14 10:32:08 -0400

    commit

--- src/main/java/py/com/ci/academy/account/boundary/AccountController.java
+++ src/main/java/py/com/ci/academy/account/boundary/AccountController.java
@@ -12,22 +12,17 @@ public class AccountController {
     AccountManager accountManager = new AccountManager();
 
     public void addAccount(int idAccount) {
-        LocalDate date;
+        LocalDate localdate;
         account.setIdAccount(idAccount);
         account.setStatus("Pending");
         account.setAmount(50000);
         for (int i = 0; i < 6; i++) {
-            date=LocalDate.now().plusMonths(i).with(lastDayOfMonth());
-            account.setExpireDate(convertToDate(date));
+            localdate=LocalDate.now().plusMonths(i).with(lastDayOfMonth());
+            account.setExpireDate(localdate);
             accountManager.addAccount(account);
         }
     }
     
-    
-    public List<Account> getAll(){
-        List<Account> account = accountManager.getAll();
-        return account;
-    }
         
     public LocalDate convertToLocalDate(Date dateToConvert) {
         return dateToConvert.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
@@ -37,4 +32,21 @@ public class AccountController {
         return java.sql.Date.valueOf(dateToConvert);
     }
 
+
+    public List<Account> getAll(){
+        List<Account> accounts = accountManager.getAll();
+        return accounts;
+    }
+    
+    public int updateAccount(Account account) {
+        int ban = accountManager.updateById(account);
+        return ban;
+    }
+    
+    public int deleteAccount(Account account) {
+        int ban = accountManager.deleteById(account);
+        return ban;
+    }
+
 }
+
