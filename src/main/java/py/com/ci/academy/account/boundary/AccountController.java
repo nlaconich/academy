24870f1116b0d98acbfab6407e39912af0c2e57c
jespinolas <jespinola@consultoresinformaicos.com.py>
2021-09-14 10:32:08 -0400

    commit

--- src/main/java/py/com/ci/academy/account/boundary/AccountController.java
+++ src/main/java/py/com/ci/academy/account/boundary/AccountController.java
@@ -1,9 +1,7 @@
 package py.com.ci.academy.account.boundary;
-
 import java.time.LocalDate;
 import java.time.ZoneId;
 import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import py.com.ci.academy.account.entities.Account;
@@ -13,20 +11,24 @@ public class AccountController {
     Account account = new Account();
     AccountManager accountManager = new AccountManager();
 
-    public void addAccount(int idInscription) {
+    public void addAccount(int idAccount) {
         LocalDate date;
-//        date.add(LocalDate.now().plusMonths(i).with(lastDayOfMonth()));
-//      date_account,remark,status,id_inscription,amount
+        account.setIdAccount(idAccount);
+        account.setStatus("Pending");
+        account.setAmount(50000);
         for (int i = 0; i < 6; i++) {
             date=LocalDate.now().plusMonths(i).with(lastDayOfMonth());
             account.setExpireDate(convertToDate(date));
-            account.setStatus("Pending");
-            account.setAmount(50000);
+            accountManager.addAccount(account);
         }
-
     }
-
-    //-------------SET DATE OF ACCOUNT
+    
+    
+    public List<Account> getAll(){
+        List<Account> account = accountManager.getAll();
+        return account;
+    }
+        
     public LocalDate convertToLocalDate(Date dateToConvert) {
         return dateToConvert.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
     }
