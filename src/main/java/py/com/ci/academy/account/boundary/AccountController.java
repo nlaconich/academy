a38c63dac6c859a5892d2eedea64a6e6f5ed29e4
jespinolas <jespinola@consultoresinformaicos.com.py>
2021-09-14 10:32:08 -0400

    commit

--- src/main/java/py/com/ci/academy/account/boundary/AccountController.java
+++ src/main/java/py/com/ci/academy/account/boundary/AccountController.java
@@ -1,9 +1,7 @@
 package py.com.ci.academy.account.boundary;
-
 import java.time.LocalDate;
 import java.time.ZoneId;
 import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import py.com.ci.academy.account.entities.Account;
@@ -13,26 +11,41 @@ public class AccountController {
     Account account = new Account();
     AccountManager accountManager = new AccountManager();
 
-    public void addAccount(int idInscription) {
-        LocalDate date;
-//        date.add(LocalDate.now().plusMonths(i).with(lastDayOfMonth()));
-//      date_account,remark,status,id_inscription,amount
+    public void addAccount(int idAccount) {
+        LocalDate localdate;
+        account.setIdAccount(idAccount);
+        account.setStatus("Pending");
+        account.setAmount(50000);
         for (int i = 0; i < 6; i++) {
-            date=LocalDate.now().plusMonths(i).with(lastDayOfMonth());
-            account.setExpireDate(convertToDate(date));
-            account.setStatus("Pending");
-            account.setAmount(50000);
+            localdate=LocalDate.now().plusMonths(i).with(lastDayOfMonth());
+            account.setExpireDate(localdate);
+            accountManager.addAccount(account);
         }
-
     }
-
-    //-------------SET DATE OF ACCOUNT
+    
+        
     public LocalDate convertToLocalDate(Date dateToConvert) {
         return dateToConvert.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
     }
 
     public static Date convertToDate(LocalDate dateToConvert) {
         return java.sql.Date.valueOf(dateToConvert);
+    }
+
+
+    public List<Account> getAll(){
+        List<Account> accounts = accountManager.getAll();
+        return accounts;
+    }
+    
+    public int updateAccount(Account account) {
+        int ban = accountManager.updateById(account);
+        return ban;
+    }
+    
+    public int deleteAccount(Account account) {
+        int ban = accountManager.deleteById(account);
+        return ban;
     }
 
 }
